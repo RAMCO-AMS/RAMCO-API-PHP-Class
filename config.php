1f38e327d86baedf8d35727b6780a56b134843bb
Dave Conroy <dconroy@gmail.com>
2015-03-20 22:12:12 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// PEM file is the cert file I use to validate certificate authenticity.
+const PEM_FILE = 'cacert.pem';
+
+// API URL is the same for every single API v2 request.
+const API_URL = 'https://api.ramcoams.com/api/v2/';
+
+// This is a fake, non-working API key, yours has to be substituted here.
+const API_KEY = 'NAR-Stage-Fake-11bd0b401ae37118509d99949587f3ec0122e3de';
+?>
